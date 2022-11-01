c8991fb37dab8b3ff4477d266e6816240fa567cc
ArpadKovacs <95.arpadkov@gmail.com>
2022-11-01 16:26:47 +0100

    adds new icons, display for ToBattery & FromBattery, display dont always show correct values

--- SenecTray/MainWindow.cs
+++ SenecTray/MainWindow.cs
@@ -16,6 +16,10 @@ namespace SenecTray
         private static HttpClient client = new HttpClient();
         private string token;
         private int BatteryState = 0;
+        private int ToBattery = 0;
+        private int FromBattery = 0;
+        private float ToGrid = 0;
+        private float FromGrid = 0;
         private BackgroundWorker backgroundWorker1 = new BackgroundWorker();  
         //backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
 
@@ -73,9 +77,19 @@ namespace SenecTray
 
             var responseContent = response.Content.ReadAsStringAsync().Result;
             var json_data = JObject.Parse(responseContent);
+
             string state = json_data["aktuell"]["speicherfuellstand"]["wert"].ToObject<string>();
+            string to_battery = json_data["aktuell"]["speicherbeladung"]["wert"].ToObject<string>();
+            string from_battery = json_data["aktuell"]["speicherentnahme"]["wert"].ToObject<string>();
+            string to_grid = json_data["aktuell"]["netzeinspeisung"]["wert"].ToObject<string>();
+            string from_grid = json_data["aktuell"]["netzbezug"]["wert"].ToObject<string>();
 
             BatteryState = int.Parse(state.Split('.').First());
+            ToBattery = (int) Math.Round(float.Parse(to_battery) / 1000);
+            FromBattery = (int) Math.Round(float.Parse(from_battery) / 1000);
+            ToGrid = float.Parse(to_grid) / 1000;
+            FromGrid = float.Parse(from_grid) / 1000;
+            
 
             //result = response.Content.ReadAsStringAsync().Result;
         }
@@ -83,41 +97,96 @@ namespace SenecTray
         public void update(object sender, EventArgs e)
         {
             this.label1.Text = BatteryState.ToString();
-            notifyIcon1.Text = "Battery: " + BatteryState.ToString() + " %" + "\n" + DateTime.Now;
+            notifyIcon1.Text = "Battery: " + BatteryState.ToString() + " %" + "\n"
+                + "Battery charge:     " + ToBattery.ToString() + " kW" + "\n"
+                + "Battery discharge: " + FromBattery.ToString() + " kW" + "\n"
+                + DateTime.Now;
 
             if (BatteryState == 100)
             {
-                notifyIcon1.Icon = Properties.Resources.full;
+                notifyIcon1.Icon = Properties.Resources.full_total;
                 return;
             }
 
             if (BatteryState > 80)
             {
-                notifyIcon1.Icon = Properties.Resources.stage4;
-                return;
+                if (ToBattery > FromBattery)
+                {
+                    notifyIcon1.Icon = Properties.Resources.state4_charge;
+                    return;
+                }
+
+                else
+                {
+                    notifyIcon1.Icon = Properties.Resources.state4_discharge;
+                    return;
+                }
+                
             }
 
             if (BatteryState > 60)
             {
-                notifyIcon1.Icon = Properties.Resources.stage3;
-                return;
+                if (ToBattery > FromBattery)
+                {
+                    notifyIcon1.Icon = Properties.Resources.state3_charge;
+                    return;
+                }
+
+                else
+                {
+                    notifyIcon1.Icon = Properties.Resources.state3_discharge;
+                    return;
+                }
             }
 
             if (BatteryState > 40)
             {
-                notifyIcon1.Icon = Properties.Resources.stage2;
-                return;
+                if (ToBattery > FromBattery)
+                {
+                    notifyIcon1.Icon = Properties.Resources.state2_charge;
+                    return;
+                }
+
+                else
+                {
+                    notifyIcon1.Icon = Properties.Resources.state2_discharge;
+                    return;
+                }
             }
 
             if (BatteryState > 20)
             {
-                notifyIcon1.Icon = Properties.Resources.stage1;
-                return;
+                if (ToBattery > FromBattery)
+                {
+                    notifyIcon1.Icon = Properties.Resources.state1_charge;
+                    return;
+                }
+
+                else
+                {
+                    notifyIcon1.Icon = Properties.Resources.state1_discharge;
+                    return;
+                }
             }
 
-            if (BatteryState < 20)
+            if (BatteryState > 0)
             {
-                notifyIcon1.Icon = Properties.Resources.empty;
+                if (ToBattery > FromBattery)
+                {
+                    notifyIcon1.Icon = Properties.Resources.state0_charge;
+                    return;
+                }
+
+                else
+                {
+                    notifyIcon1.Icon = Properties.Resources.state0_discharge;
+                    return;
+                }
+            }
+
+            if (BatteryState == 0)
+            {
+                notifyIcon1.Icon = Properties.Resources.empty_total;
                 return;
             }
         }
